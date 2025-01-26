47e627a937bfed8bff013c70471d40877902f396
karabo <mawaiskara@gmail.com>
2025-01-26 23:59:03 +0200

    editted login function

--- auth.py
+++ auth.py
@@ -1,5 +1,6 @@
 import pyrebase
 from Firebase import FirebaseConfig
+from database import save
 
 firebase = pyrebase.initialize_app(FirebaseConfig)
 auth=firebase.auth()
@@ -8,20 +9,16 @@ def login():
     print("login here!")
     email = input("enter your email here> ")
     password = input("input a password> ")
-    while True:
-        role = input("input your role here, choose from either mentor or peer>")
-        if role.lower() in ("mentor", "peer"):
-            break
     try:
-        login = auth.sign_in_with_email_and_password(email, password)
+        user = auth.sign_in_with_email_and_password(email, password)
         print("login successful!")
-        return email, password, role
     except:
         print("something went wrong, check spelling and try again")
     return
 
 def signup():
     print("signup here!")
+    name = input("enter your full name> ")
     email = input("enter your email here> ")
     password = input("input a password> ")
     while True:
@@ -31,18 +28,20 @@ def signup():
     try:
         user = auth.create_user_with_email_and_password(email, password)
         print("signup successful!")
-        return email, password, role
+        save(name, email, password, role)
     except:
         print("email already exists")
     return
 
-choice = input("are you a new user y/n> ")
-if not choice in ["y", "n"]:
-    while True:
-        choice = input("please choose from y/n> ")
-        if choice in ["y", "n"]:
-            break
-if choice == "y":
-    signup()
-else:
-    login()
+def check():
+    choice = input("are you a new user y/n> ")
+    if not choice in ["y", "n"]:
+        while True:
+            choice = input("please choose from y/n> ")
+            if choice in ["y", "n"]:
+                break
+    if choice == "y":
+        signup()
+    else:
+        login()
+    return
